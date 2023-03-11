3dc97aa47142e89731d0e41ba1d35a3d3493b12d
Zanetaa <91408269+Zanetaa@users.noreply.github.com>
2023-03-11 21:52:56 +0100

    dílčí úkol č.11

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -3,6 +3,7 @@ import './style.css';
 import { Lights } from '../Lights';
 import { Climate } from '../Climate';
 import { Blinds } from '../Blinds';
+import { Energy } from '../Energy';
 
 export const Dashboard = ({ data }) => {
   return (
@@ -13,41 +14,8 @@ export const Dashboard = ({ data }) => {
         temperature={data.temperature}
         temperature={data.huidity}
       />
-      <Blinds key={data.name} name={data.name} />
-
-      <div className="blinds">
-        <div className="blinds__icon">
-          <img src="./images/blinds-open.svg" />
-        </div>
-        <div className="blinds__name">Žaluzie</div>
-        <div className="blinds__controls">
-          <button className="button button--active">Otevřeno</button>
-          <button className="button">Zavřeno</button>
-        </div>
-      </div>
-
-      <div className="energy">
-        <div className="energy__source">
-          <div className="energy__icon">
-            <img src="./images/electricity.svg" />
-          </div>
-          <div className="energy__consumption">
-            <div className="energy__description">Elektřina</div>
-            <div className="energy__value">36.7 kW</div>
-          </div>
-        </div>
-        <div className="energy__source">
-          <div className="energy__icon">
-            <img src="./images/water.svg" />
-          </div>
-          <div className="energy__consumption">
-            <div className="energy__description">Voda</div>
-            <div className="energy__value">
-              14.1 m<sup>3</sup>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Blinds blinds={data.blinds} />
+      <Energy electricity={data.electricity} water={data.water} />
     </main>
   );
 };
